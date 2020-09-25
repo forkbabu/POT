12a2095e73e69fce810fb909d376e6b4cbf1acd3
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-25 19:59:58 +0530

    Update __init__.py

--- ot/torch/__init__.py
+++ ot/torch/__init__.py
@@ -13,7 +13,7 @@ from . import utils
 from . import lp
 
 from .utils import dist, unif
-from .lp import ot_loss, ot_solve, OptimalTransportLossFunction
+from .lp import ot_loss, ot_solve, OptimalTransportLossFunction,GromovWassersteinLossFunction
 
-__all__ = ['dist','unif','ot_loss',"ot_solve"]
+__all__ = ['dist','unif','ot_loss',"ot_solve","otgw_solve","otgw_loss"]
 
