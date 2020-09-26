53b49ed4aac09bd5125a8eed232a2f5c9b970d46
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-09-26 16:14:27 +0530

    Update lp.py

--- ot/torch/lp.py
+++ ot/torch/lp.py
@@ -86,7 +86,7 @@ class GromovWassersteinLossFunction(Function):
         grad_T = None
 
         if ctx.needs_input_grad[0]:
-            grad_T = grad_T0[0]
+            grad_T = grad_T0
         return grad_T,None,None,None
 
 
